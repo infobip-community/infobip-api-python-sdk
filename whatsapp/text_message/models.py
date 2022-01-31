52debf4923d22386765af1351c98a9bcad1095d0
Luka Kilić <57562025+kilicluka@users.noreply.github.com>
2022-01-31 10:07:39 +0100

    Add more tests for text msg (#3)
    * Add min_length and default None to Optional fields
    * Add validation tests for text message sending

--- whatsapp/text_message/models.py
+++ whatsapp/text_message/models.py
@@ -5,13 +5,13 @@ from whatsapp.core.models import CamelCaseModel
 
 
 class Content(CamelCaseModel):
-    text: constr(max_length=4096)
+    text: constr(min_length=1, max_length=4096)
     preview_url: Optional[StrictBool] = False
-    callback_data: Optional[constr(max_length=4000)]
+    callback_data: Optional[constr(max_length=4000)] = None
 
 
 class MessageBody(CamelCaseModel):
-    from_number: constr(max_length=24) = Field(alias="from")
-    to: constr(max_length=24)
-    message_id: Optional[constr(max_length=50)]
+    from_number: constr(min_length=1, max_length=24) = Field(alias="from")
+    to: constr(min_length=1, max_length=24)
+    message_id: Optional[constr(max_length=50)] = None
     content: Content
