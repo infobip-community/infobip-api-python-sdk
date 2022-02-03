d4c5efaccc1ce8012ba07b9ad1568bdf2da29e3c
Luka Kilic <kilic.luka@gmail.com>
2022-02-03 22:23:22 +0100

    Add integration test for text_message

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ from random import choice
 from string import ascii_letters
 
 import pytest
+import requests
 from pydantic_factories import ModelFactory
 
 from whatsapp.models.core import Authentication
@@ -18,3 +19,37 @@ class AuthenticationFactory(ModelFactory):
 @pytest.fixture
 def authentication():
     return AuthenticationFactory.build()
+
+
+class HttpTestClient:
+    def __init__(self, url):
+        self.url = url
+
+    def post(self, endpoint, body):
+        return requests.post(url=f"{self.url}" + endpoint, json=body)
+
+
+@pytest.fixture
+def http_test_client():
+    def _get_http_test_client(url):
+        return HttpTestClient(url)
+
+    return _get_http_test_client
+
+
+@pytest.fixture
+def bad_request_response():
+    return {
+        "requestError": {
+            "serviceException": {
+                "messageId": "BAD_REQUEST",
+                "text": "Bad request",
+                "validationErrors": {
+                    "content.text": [
+                        "size must be between 1 and 4096",
+                        "must not be blank",
+                    ]
+                },
+            }
+        }
+    }
--- tests/test_text_message.py
+++ tests/test_text_message.py
@@ -3,6 +3,7 @@ from pydantic.error_wrappers import ValidationError
 from pydantic_factories import ModelFactory
 
 from tests.conftest import get_random_string
+from whatsapp.client import WhatsappClient
 from whatsapp.models.core import MessageBody
 from whatsapp.models.text_message import TextMessageBody
 
@@ -33,3 +34,18 @@ def test_when_content_preview_url_is_invalid__validation_error_is_raised(preview
         TextMessageBodyFactory.build(
             **{"content": {"text": "text", "previewUrl": preview_url}}
         )
+
+
+def test_when_bad_request_is_sent__400_response_is_received(
+    httpserver, http_test_client, bad_request_response
+):
+    httpserver.expect_request(
+        "/whatsapp/1/message/text", method="POST"
+    ).respond_with_json(bad_request_response)
+
+    whatsapp_client = WhatsappClient.from_provided_client(
+        http_test_client(httpserver.url_for("/"))
+    )
+    response = whatsapp_client.send_text_message(TextMessageBodyFactory.build())
+
+    assert response.json() == bad_request_response
