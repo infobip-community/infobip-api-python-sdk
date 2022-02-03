f2b143864cd6d49e64bf9d702abfcc8082571932
Luka Kilic <kilic.luka@gmail.com>
2022-02-03 11:13:18 +0100

    Remove duplicated data from TextMessageBody

--- whatsapp/text_message/models.py
+++ whatsapp/text_message/models.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from pydantic import Field, StrictBool, constr
+from pydantic import StrictBool, constr
 
 from whatsapp.core.models import CamelCaseModel, MessageBody
 
@@ -8,11 +8,7 @@ from whatsapp.core.models import CamelCaseModel, MessageBody
 class Content(CamelCaseModel):
     text: constr(min_length=1, max_length=4096)
     preview_url: Optional[StrictBool] = False
-    callback_data: Optional[constr(max_length=4000)] = None
 
 
 class TextMessageBody(MessageBody):
-    from_number: constr(min_length=1, max_length=24) = Field(alias="from")
-    to: constr(min_length=1, max_length=24)
-    message_id: Optional[constr(max_length=50)] = None
     content: Content
