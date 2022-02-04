af2ed03089ced3dd5826bada4c7f9e38fb3a9759
Luka Kilić <57562025+kilicluka@users.noreply.github.com>
2022-02-04 21:43:00 +0100

    Add text message integration tests (#7)
    Co-authored-by: Art Vandelay <start.vandelay@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,11 @@
+import json
 from random import choice
 from string import ascii_letters
 
 import pytest
+import requests
 from pydantic_factories import ModelFactory
+from werkzeug.wrappers.response import Response
 
 from whatsapp.models.core import Authentication
 
@@ -18,3 +21,97 @@ class AuthenticationFactory(ModelFactory):
 @pytest.fixture
 def authentication():
     return AuthenticationFactory.build()
+
+
+class HttpTestClient:
+    def __init__(self, url):
+        self.url = url
+
+    def post(self, endpoint, body):
+        return requests.post(url=f"{self.url}" + endpoint, json=body)
+
+
+@pytest.fixture
+def http_test_client():
+    def _get_http_test_client(url):
+        return HttpTestClient(url)
+
+    return _get_http_test_client
+
+
+@pytest.fixture
+def ok_content():
+    return {
+        "to": "441134960001",
+        "messageCount": 1,
+        "messageId": "a28dd97c-1ffb-4fcf-99f1-0b557ed381da",
+        "status": {
+            "groupId": 1,
+            "groupName": "PENDING",
+            "id": 7,
+            "name": "PENDING_ENROUTE",
+            "description": "Message sent to next instance",
+        },
+    }
+
+
+@pytest.fixture
+def response_ok(ok_content):
+    return Response(json.dumps(ok_content), status=200)
+
+
+@pytest.fixture
+def bad_request_content():
+    return {
+        "requestError": {
+            "serviceException": {
+                "messageId": "BAD_REQUEST",
+                "text": "Bad request",
+                "validationErrors": {
+                    "content.text": [
+                        "size must be between 1 and 4096",
+                        "must not be blank",
+                    ]
+                },
+            }
+        }
+    }
+
+
+@pytest.fixture
+def response_bad_request(bad_request_content):
+    return Response(json.dumps(bad_request_content), status=400)
+
+
+@pytest.fixture
+def unauthorized_content():
+    return {
+        "requestError": {
+            "serviceException": {
+                "messageId": "UNAUTHORIZED",
+                "text": "Invalid login details",
+            }
+        }
+    }
+
+
+@pytest.fixture
+def response_unauthorized(unauthorized_content):
+    return Response(json.dumps(unauthorized_content), status=401)
+
+
+@pytest.fixture
+def too_many_requests_content():
+    return {
+        "requestError": {
+            "serviceException": {
+                "messageId": "TOO_MANY_REQUESTS",
+                "text": "Too many requests",
+            }
+        }
+    }
+
+
+@pytest.fixture
+def response_too_many_requests(too_many_requests_content):
+    return Response(json.dumps(too_many_requests_content), status=429)
--- tests/test_text_message.py
+++ tests/test_text_message.py
@@ -1,10 +1,20 @@
+from http import HTTPStatus
+
 import pytest
 from pydantic.error_wrappers import ValidationError
 from pydantic_factories import ModelFactory
 
 from tests.conftest import get_random_string
-from whatsapp.models.core import MessageBody
+from whatsapp.client import WhatsappClient
+from whatsapp.models.core import (
+    MessageBody,
+    WhatsappResponse,
+    WhatsappResponseError,
+    WhatsappResponseOK,
+)
 from whatsapp.models.text_message import TextMessageBody
+
+TEXT_MESSAGE_ENDPOINT = "/whatsapp/1/message/text"
 
 
 class TextMessageBodyFactory(ModelFactory):
@@ -33,3 +43,69 @@ def test_when_content_preview_url_is_invalid__validation_error_is_raised(preview
         TextMessageBodyFactory.build(
             **{"content": {"text": "text", "previewUrl": preview_url}}
         )
+
+
+def test_send_text_message_with_provided_client__returns_raw_response(
+    httpserver, http_test_client, ok_content, response_ok
+):
+    httpserver.expect_request(
+        TEXT_MESSAGE_ENDPOINT, method="POST"
+    ).respond_with_response(response_ok)
+
+    whatsapp_client = WhatsappClient.from_provided_client(
+        http_test_client(httpserver.url_for("/"))
+    )
+    response = whatsapp_client.send_text_message(TextMessageBodyFactory.build())
+
+    assert isinstance(response, WhatsappResponse) is False
+    assert response.status_code == 200
+    assert response.json() == ok_content
+
+
+@pytest.mark.parametrize(
+    "raw_response,response_object,status_code,response_body",
+    [
+        ("response_ok", WhatsappResponseOK, 200, "ok_content"),
+        ("response_bad_request", WhatsappResponseError, 400, "bad_request_content"),
+        ("response_unauthorized", WhatsappResponseError, 401, "unauthorized_content"),
+        (
+            "response_too_many_requests",
+            WhatsappResponseError,
+            429,
+            "too_many_requests_content",
+        ),
+    ],
+)
+def test_send_text_message_with_auth_params__returns_whatsapp_response(
+    httpserver,
+    http_test_client,
+    raw_response,
+    response_object,
+    status_code,
+    response_body,
+    request,
+):
+    raw_response_fixture = request.getfixturevalue(raw_response)
+    response_body_fixture = request.getfixturevalue(response_body)
+
+    httpserver.expect_request(
+        TEXT_MESSAGE_ENDPOINT, method="POST"
+    ).respond_with_response(raw_response_fixture)
+
+    server_url = httpserver.url_for("/")
+    whatsapp_client = WhatsappClient.from_auth_params(
+        {"base_url": server_url, "api_key": "secret"}
+    )
+    response = whatsapp_client.send_text_message(TextMessageBodyFactory.build())
+    response_dict_cleaned = response.dict(by_alias=True, exclude_unset=True)
+    raw_response = response_dict_cleaned.pop("rawResponse")
+
+    expected_response_dict = {
+        **response_body_fixture,
+        "statusCode": HTTPStatus(status_code),
+    }
+
+    assert isinstance(response, response_object)
+    assert response.status_code == status_code
+    assert response_dict_cleaned == expected_response_dict
+    assert raw_response is not None
