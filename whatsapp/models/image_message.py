d342602f69e9d832afe1f28cccfbaf0c7873bb64
Art Vandelay <start.vandelay@gmail.com>
2022-02-05 11:26:53 +0100

    Added Send Whatsapp Image Message endpoint

---
+++ whatsapp/models/image_message.py
@@ -0,0 +1,14 @@
+from typing import Optional
+
+from pydantic import AnyUrl, constr
+
+from whatsapp.models.core import CamelCaseModel, MessageBody
+
+
+class Content(CamelCaseModel):
+    media_url: AnyUrl
+    caption: Optional[constr(max_length=3000)] = None
+
+
+class ImageMessageBody(MessageBody):
+    content: Content
