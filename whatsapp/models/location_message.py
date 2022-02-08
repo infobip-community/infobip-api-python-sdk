1a0b82e88fbcda5f0a9e75d9cfda1a9d5889f717
Art Vandelay <start.vandelay@gmail.com>
2022-02-08 10:36:22 +0100

    Updated location message model

--- whatsapp/models/location_message.py
+++ whatsapp/models/location_message.py
@@ -8,8 +8,8 @@ from whatsapp.models.core import CamelCaseModel, MessageBody
 class Content(CamelCaseModel):
     latitude: confloat(ge=-90, le=90)
     longitude: confloat(ge=-180, le=180)
-    name: Optional[constr(min_length=1, max_length=1000)]
-    address: Optional[constr(min_length=1, max_length=1000)]
+    name: Optional[constr(min_length=1, max_length=1000)] = None
+    address: Optional[constr(min_length=1, max_length=1000)] = None
 
 
 class LocationMessageBody(MessageBody):
