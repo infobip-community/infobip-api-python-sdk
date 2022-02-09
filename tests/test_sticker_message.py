bf9a17c461474310a59dcbb1bacd96ab0387ba9e
Luka Kilic <kilic.luka@gmail.com>
2022-02-09 09:41:21 +0100

    Fix sticker message media_url and tests

--- tests/test_sticker_message.py
+++ tests/test_sticker_message.py
@@ -15,7 +15,10 @@ def test_when_content_is_invalid__validation_error_is_raised(content):
         StickerMessageBodyFactory.build(**{"content": content})
 
 
-@pytest.mark.parametrize("media_url", [None, "", {}, get_random_string(4097)])
+@pytest.mark.parametrize(
+    "media_url",
+    [None, "", {}, "ftp://myfile.com", f"http://myfile.com/{get_random_string(2031)}"],
+)
 def test_when_content_media_url_is_invalid__validation_error_is_raised(media_url):
     with pytest.raises(ValidationError):
         StickerMessageBodyFactory.build(**{"content": {"mediaUrl": media_url}})
