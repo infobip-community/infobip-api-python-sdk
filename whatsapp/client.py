573fcf2c41be5ec44e72f230022f194f2bf562db
Dino Lozina <Dino.Lozina@live.com>
2022-02-15 12:22:26 +0100

    Changed send template message

--- whatsapp/client.py
+++ whatsapp/client.py
@@ -337,7 +337,8 @@ class WhatsAppChannel:
         :return:
         """
 
-        message = self.validate_message_body(message, TemplateMassageBody)
+        if not isinstance(message, TemplateMassageBody):
+            message = TemplateMassageBody(**message)
 
         return self._client.post(
             self.SEND_MESSAGE_URL_TEMPLATE + "template",
