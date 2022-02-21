e43184fd38453937437f014a49c56da4d489b9ad
Luka Kilic <kilic.luka@gmail.com>
2022-02-21 23:04:16 +0100

    Cover validate_auth_params with a test

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,6 +2,7 @@ import pytest
 from pydantic.error_wrappers import ValidationError
 from pydantic_factories import ModelFactory
 
+from infobip_channels.whatsapp.client import WhatsAppChannel
 from infobip_channels.whatsapp.models.core import Authentication, MessageBody
 from tests.conftest import get_random_string
 
@@ -35,6 +36,13 @@ def test_base_url_in_different_forms__validation_passes(base_url):
         Authentication(base_url=base_url, api_key="api_key")
     except ValidationError:
         pytest.fail("Unexpected ValidationError raised")
+
+
+def test_validate_auth_params_returns_authentication_instance():
+    auth_object = WhatsAppChannel.validate_auth_params(
+        "https://123.api.infobip.com", "api_key"
+    )
+    assert isinstance(auth_object, Authentication) is True
 
 
 @pytest.mark.parametrize("from_number", [None, "", {}, get_random_string(25)])
