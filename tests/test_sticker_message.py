5eb80a7969aa1e0d23937db46077da5e27066ca5
Luka Kilic <kilic.luka@gmail.com>
2022-02-24 22:54:21 +0100

    Add positive sticker message test

--- tests/test_sticker_message.py
+++ tests/test_sticker_message.py
@@ -2,6 +2,7 @@ import pytest
 from pydantic.error_wrappers import ValidationError
 
 from infobip_channels.whatsapp.models.core import MessageBody
+from infobip_channels.whatsapp.models.sticker_message import StickerMessageBody
 from tests.conftest import StickerMessageBodyFactory, get_random_string
 
 
@@ -22,3 +23,19 @@ def test_when_content_is_invalid__validation_error_is_raised(content):
 def test_when_content_media_url_is_invalid__validation_error_is_raised(media_url):
     with pytest.raises(ValidationError):
         StickerMessageBodyFactory.build(**{"content": {"mediaUrl": media_url}})
+
+
+def test_when_input_data_is_valid__validation_error_is_not_raised():
+    try:
+        StickerMessageBody(
+            **{
+                "from": "441134960000",
+                "to": "38598451987",
+                "messageId": "a28dd97c-1ffb-4fcf-99f1-0b557ed381da",
+                "content": {"media_url": "https://some.file.com"},
+                "callbackData": "Callback data",
+                "notify_url": "https://www.example.com/whatsapp",
+            },
+        )
+    except ValidationError:
+        pytest.fail("Unexpected ValidationError raised")
