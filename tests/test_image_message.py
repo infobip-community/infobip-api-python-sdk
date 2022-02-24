510a857ed574a856dffba156e15a6e375cf6e760
Luka Kilic <kilic.luka@gmail.com>
2022-02-24 23:20:06 +0100

    Add positive test for send image

--- tests/test_image_message.py
+++ tests/test_image_message.py
@@ -2,6 +2,7 @@ import pytest
 from pydantic.error_wrappers import ValidationError
 
 from infobip_channels.whatsapp.models.core import MessageBody
+from infobip_channels.whatsapp.models.image_message import ImageMessageBody
 from tests.conftest import ImageMessageBodyFactory, get_random_string
 
 
@@ -9,7 +10,7 @@ def test_image_message_body__is_an_instance_of_message_body():
     assert isinstance(ImageMessageBodyFactory.build(), MessageBody) is True
 
 
-@pytest.mark.parametrize("content", [None, "", {}])
+@pytest.mark.parametrize("content", [None, "", {}, {"caption": "the image"}])
 def test_when_content_is_invalid__validation_error_is_raised(content):
     with pytest.raises(ValidationError):
         ImageMessageBodyFactory.build(**{"content": content})
@@ -30,3 +31,18 @@ def test_when_content_caption_is_invalid__validation_error_is_raised(caption):
         ImageMessageBodyFactory.build(
             **{"content": {"mediaUrl": "http://mymedia.com", "caption": caption}}
         )
+
+
+def test_when_input_data_is_valid__validation_error_is_not_raised():
+    try:
+        ImageMessageBody(
+            **{
+                "from": "441134960000",
+                "to": "38598451987",
+                "messageId": "a28dd97c-1ffb-4fcf-99f1-0b557ed381da",
+                "content": {"media_url": "http://theimage.com"},
+                "notifyUrl": "https://notify.me",
+            }
+        )
+    except ValidationError:
+        pytest.fail("Unexpected ValidationError raised")
