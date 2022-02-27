c13aaa8d9bdadf0a07ac3336823205089035687c
Luka Kilić <57562025+kilicluka@users.noreply.github.com>
2022-02-27 12:17:28 +0100

    Add document message postitive test case (#40)

--- tests/whatsapp/test_document_message.py
+++ tests/whatsapp/test_document_message.py
@@ -2,6 +2,7 @@ import pytest
 from pydantic.error_wrappers import ValidationError
 
 from infobip_channels.whatsapp.models.body.core import MessageBody
+from infobip_channels.whatsapp.models.body.document_message import DocumentMessageBody
 from tests.whatsapp.conftest import DocumentMessageBodyFactory, get_random_string
 
 
@@ -9,7 +10,9 @@ def test_document_message_body__is_an_instance_of_message_body():
     assert isinstance(DocumentMessageBodyFactory.build(), MessageBody) is True
 
 
-@pytest.mark.parametrize("content", [None, "", {}])
+@pytest.mark.parametrize(
+    "content", [None, "", {}, {"caption": "caption", "filename": "test"}]
+)
 def test_when_content_is_invalid__validation_error_is_raised(content):
     with pytest.raises(ValidationError):
         DocumentMessageBodyFactory.build(**{"content": content})
@@ -38,3 +41,23 @@ def test_when_content_filename_is_invalid__validation_error_is_raised(filename):
         DocumentMessageBodyFactory.build(
             **{"content": {"mediaUrl": "http://mymedia.com", "filename": filename}}
         )
+
+
+def test_when_input_data_is_valid__validation_error_is_not_raised():
+    try:
+        DocumentMessageBody(
+            **{
+                "from": "441134960000",
+                "to": "38598451987",
+                "messageId": "a28dd97c-1ffb-4fcf-99f1-0b557ed381da",
+                "content": {
+                    "media_url": "http://document.com",
+                    "caption": "cool tile",
+                    "filename": "doc.pdf",
+                },
+                "notifyUrl": "https://notify.me",
+                "callbackData": "Some callback data",
+            }
+        )
+    except ValidationError:
+        pytest.fail("Unexpected ValidationError raised")
