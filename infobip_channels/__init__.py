4ec926f982ebcf435ce1b6001f5cf9ca40945d0e
Dino Lozina <dino.lozina@live.com>
2022-06-08 09:58:32 +0200

    Changed README.md and updated version number

--- infobip_channels/__init__.py
+++ infobip_channels/__init__.py
@@ -1,7 +1,15 @@
+from .email.channel import EmailChannel
 from .mms.channel import MMSChannel
 from .rcs.channel import RCSChannel
 from .sms.channel import SMSChannel
 from .web_rtc.channel import WebRtcChannel
 from .whatsapp.channel import WhatsAppChannel
 
-__all__ = ["WhatsAppChannel", "WebRtcChannel", "MMSChannel", "RCSChannel", "SMSChannel"]
+__all__ = [
+    "WhatsAppChannel",
+    "WebRtcChannel",
+    "MMSChannel",
+    "RCSChannel",
+    "SMSChannel",
+    "EmailChannel",
+]
