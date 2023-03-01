0b00bed6c565aa7e5c3e7c795ce1195e83372f6a
Erick Daniel Corona <edcoronag@gmail.com>
2023-02-28 18:47:34 -0600

    Add missing mccmnc and callbackData

--- infobip_channels/sms/models/response/outbound_delivery_reports.py
+++ infobip_channels/sms/models/response/outbound_delivery_reports.py
@@ -1,8 +1,13 @@
-from typing import List
+from typing import List, Optional
 
 from infobip_channels.core.models import ResponseBase
 from infobip_channels.sms.models.response.core import CoreResult
 
 
+class SMSReport(CoreResult):
+    mcc_mnc: Optional[str] = None
+    callback_data: Optional[str] = None
+
+
 class OutboundDeliveryReportsResponse(ResponseBase):
-    results: List[CoreResult]
+    results: List[SMSReport]
